10ff84ee17e4353c68f9983b55d297140f0b7700
rafiananta1234 <rafiananta1234@student.ub.ac.id>
2026-02-10 13:49:24 +0700

    m

--- bujurSangkar.java
+++ bujurSangkar.java
@@ -1,1 +1,11 @@
-
+public class bujurSangkar extends bidangDuaDimensi{
+  double sisi;
+  public bujurSangkar(String nama, double sisi){
+    super(nama);
+    this.sisi = sisi;
+  }
+  @Override 
+  public double luas(double sisi){
+    return sisi * sisi;
+  }
+}

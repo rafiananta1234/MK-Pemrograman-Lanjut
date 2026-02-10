6e3152d8b49d2404fa1ed876b662fa488b374f20
rafiananta1234 <rafiananta1234@student.ub.ac.id>
2026-02-10 14:06:41 +0700

    Penambahan "class" setelah public, penambahan String dan double pada constructor, penambahan kurung kurawal tutup pada method luas

--- Lingkaran.java
+++ Lingkaran.java
@@ -1,10 +1,11 @@
-public Lingkaran extend bidangDuaDimensi{
+public class Lingkaran extends bidangDuaDimensi{
   double r;
 
-  public Lingkaran(nama, r){
+  public Lingkaran(String nama, double r){
     super(nama);
     this.r = r;
   }
   public double luas(){
     return 3.14 * r * r;
+  }
 }
